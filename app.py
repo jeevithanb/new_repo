bf707f24ff5711b52433fe32a87c502058241813
jeevithanb <78503935+jeevithanb@users.noreply.github.com>
2022-07-06 11:36:06 +0100

    commit changes

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
-
-@app.route("/",methods=['GET','POST'])
+@app.route("/",methods=["GET","POST"])
 
 def index():
-    return"Starting ML-Project"
 
+    return "MACHINE LEARNING PROJECT"
 
 if __name__=="__main__":
     app.run(debug=True)
